56a67ab6ac0f330e2fc0c6b97dfd6867fcc46e68
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-15 15:58:27 -0500

    favorites append on page load, code client.js cleanup

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,36 +1,34 @@
 var myApp = angular.module('myApp', []);
 
+myApp.controller('GetController', ['$scope', 'MovieService', function($scope, MovieService){
+  console.log('GetController loaded');
+  MovieService.getFavorites();
+}]);
+
 //SearchController handles search input, calling getOMDB (API get), display of search results
 myApp.controller('SearchController', ['$scope', 'MovieService', function($scope, MovieService){
   console.log('SearchController loaded');
   $scope.getOMDB = MovieService.getOMDB;
-  $scope.movieObject = MovieService.movieObject;
+  $scope.searchResult = MovieService.searchResult;
   $scope.addFavorite = MovieService.addFavorite;
 }]);
 
 //FavoriteController handles display of favorite movies
 myApp.controller('FavoriteController', ['$scope', 'MovieService', function($scope, MovieService){
   console.log('FavoriteController loaded');
-  $scope.favoritesArray = MovieService.favoritesArray;
-  console.log('favorites array in controller: ', $scope.favoritesArray);
+  $scope.movieObject = MovieService.movieObject;
+  console.log('movidObject', $scope.movieObject);
 }]);
-
-//This controller isn't doing anything just yet. I set it up to manage the base
-// favorites 'get' when the page loads, but that is not functional yet.
-myApp.controller('GetController', ['$scope', 'MovieService', function($scope, MovieService){
-  console.log('GetController loaded');
-  // $scope.getFavorites = MovieService.getFavorites;
-  // $scope.getFavorites();
-}]);
-
 
 myApp.factory('MovieService', ['$http', function($http){
 
-var favoritesArray = [];
 var movieObject = {};
+
+var searchResult = {};
 
 //HTTP POST that sends movie object and saves it in DB
 function saveFavorite(movie) {
+  console.log('passed into saveFavorite: ', movie);
   $http.post('/movies', movie).then(function(response){
     console.log('movie saved to DB');
   });
@@ -39,32 +37,32 @@ function saveFavorite(movie) {
 //HTTP GET that returns an array of movie objects from DB
 function getFavorites() {
     $http.get('/movies').then(function(response){
-      console.log('data from GET request: ', response.data);
-      // favoritesArray = response.data;
-      // console.log('logging favoritesArray', favoritesArray);
+      console.log('response.data from GET request: ', response.data);
+      movieObject.data = response.data;
     });
+}
+//getOMDB connects with the OMDB api, searches DB by movie title
+function getOMDB(newMovie){
+  console.log('what i am sending to omdb: ', newMovie);
+  var movie = newMovie;
+  $http.get('http://www.omdbapi.com/?t=' + movie.title + '&y=&plot=short&r=json').then(function(response){
+    searchResult.data = response.data;
+    console.log('here is searchResult.data ', searchResult.data);
+  }); //end get
+} //end getOMDB
+
+function addFavorite(searchResult) {
+  console.log('logging movie', searchResult.data);
+  saveFavorite(searchResult.data);
+  console.log('ran addFavorite');
+  getFavorites();
 }
 
   return {
-    favoritesArray: favoritesArray,
+    searchResult: searchResult,
     movieObject : movieObject,
     getFavorites : getFavorites,
-    //getOMDB connects with the OMDB api, searches DB by movie title
-    getOMDB : function(newMovie) {
-      console.log('what i am sending to db: ', newMovie);
-      var movie = newMovie;
-      $http.get('http://www.omdbapi.com/?t=' + movie.title + '&y=&plot=short&r=json').then(function(response){
-        movieObject.data = response.data;
-        console.log('here is movieObject.data ', movieObject.data);
-      }); //end get
-    }, //end getOMDB
-    addFavorite : function(movie) {
-      console.log('logging movie', movie.data);
-      favoritesArray.push(movie.data);
-      console.log('logging favoritesArray', favoritesArray);
-      saveFavorite(movie.data);
-      console.log('ran addFavorite');
-      getFavorites();
-    }
+    getOMDB : getOMDB,
+    addFavorite: addFavorite
   };//end return
 }]);
